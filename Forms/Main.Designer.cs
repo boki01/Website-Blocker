8abb1879c119f2db859bcb9339521d513dacb189
Borna Stefan <boki.stefan2008@gmail.com>
2023-12-13 00:17:38 +0100

    Add project files.

---
+++ Forms/Main.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Website_Blocker_from_scratch
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.data = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.add = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.Label();
+            this.text = new NullTextBox();
+            this.infoIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.infoIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data
+            // 
+            this.data.AllowUserToAddRows = false;
+            this.data.AllowUserToDeleteRows = false;
+            this.data.AllowUserToResizeColumns = false;
+            this.data.AllowUserToResizeRows = false;
+            this.data.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.data.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.data.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.data.Location = new System.Drawing.Point(0, 0);
+            this.data.Name = "data";
+            this.data.ReadOnly = true;
+            this.data.RowHeadersWidth = 30;
+            this.data.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.data.Size = new System.Drawing.Size(504, 277);
+            this.data.TabIndex = 0;
+            this.data.MouseClick += new System.Windows.Forms.MouseEventHandler(this.data_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.data);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 277);
+            this.panel1.TabIndex = 1;
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.Transparent;
+            this.add.Location = new System.Drawing.Point(418, 286);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 21);
+            this.add.TabIndex = 3;
+            this.add.Text = "Block";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // info
+            // 
+            this.info.AutoSize = true;
+            this.info.Location = new System.Drawing.Point(25, 308);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(159, 13);
+            this.info.TabIndex = 5;
+            this.info.Text = "Check if the URL contains www";
+            this.info.Visible = false;
+            // 
+            // text
+            // 
+            this.text.Cue = "www.example.com";
+            this.text.Location = new System.Drawing.Point(12, 286);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(400, 20);
+            this.text.TabIndex = 2;
+            this.text.Enter += new System.EventHandler(this.text_Enter);
+            this.text.Leave += new System.EventHandler(this.text_Leave);
+            // 
+            // infoIcon
+            // 
+            this.infoIcon.BackgroundImage = global::Website_Blocker_from_scratch.Properties.Resources.info;
+            this.infoIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.infoIcon.Location = new System.Drawing.Point(12, 307);
+            this.infoIcon.Name = "infoIcon";
+            this.infoIcon.Size = new System.Drawing.Size(15, 15);
+            this.infoIcon.TabIndex = 4;
+            this.infoIcon.TabStop = false;
+            this.infoIcon.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(504, 331);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.infoIcon);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Website Blocker";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.infoIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView data;
+        private System.Windows.Forms.Panel panel1;
+        private NullTextBox text;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.PictureBox infoIcon;
+        private System.Windows.Forms.Label info;
+    }
+}
+
---
+++ Forms/Main.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.LinkLabel;
+using System.Resources;
+
+namespace Website_Blocker_from_scratch
+{
+    public partial class Main : Form
+    {
+        string[] examples = { "www.facebook.com", "www.youtube.com", "www.twitter.com", "www.instagram.com","boki01.github.io", "web.whatsapp.com","www.example.com","www.tsrb.hr" };
+
+        string textToAdd = "\n127.0.0.1\t";
+        public Main()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            data.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            data.AllowUserToResizeRows = false;
+            data.AllowUserToResizeColumns = false;
+            data.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.AcceptButton = add;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int r = rnd.Next(examples.Length);
+            text.Cue = examples[r];
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"C:\Windows\System32\drivers\etc\hosts");
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Blocked site");
+
+                foreach (string line in lines)
+                {
+                    if (!string.IsNullOrWhiteSpace(line) && !line.StartsWith("#"))
+                    {
+
+                        string[] parts = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (parts.Length >= 2)
+                        {
+                            dt.Rows.Add(parts[1]);
+                        }
+
+                    }
+                }
+
+                data.DataSource = dt;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(
+                    "ERROR # 0: Hosts file not found! The file is included with Windows.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                Application.Exit();
+            }
+            
+        }
+        private void update()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Windows\System32\drivers\etc\hosts");
+            lines = System.IO.File.ReadAllLines(@"C:\Windows\System32\drivers\etc\hosts");
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Blocked site");
+
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line) && !line.StartsWith("#")) 
+                {
+                    string[] parts = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries); 
+                    if (parts.Length >= 2)
+                    {
+                        dt.Rows.Add(parts[1]);
+                    }
+                }
+            }
+
+            data.DataSource = dt;
+        }
+        private bool IsParameterInDataGrid(string parameter)
+        {
+            
+            foreach (DataGridViewRow row in data.Rows)
+            {
+                
+                if (row.Cells["Blocked site"].Value.ToString() == parameter)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if (text.Text != "" && !IsParameterInDataGrid(text.Text))
+            {
+                
+                string[] urls = text.Text.Split(' ');
+
+                
+                string firstUrl = urls[0];
+
+                if (IsParameterInDataGrid(firstUrl))
+                {
+                    text.Text = "";
+                    return;
+                }
+
+                try
+                {
+                    File.AppendAllText(@"C:\Windows\System32\drivers\etc\hosts", textToAdd + firstUrl);
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(
+                        "ERROR # 1: The program should be run under admin privileges!",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                    Application.Exit();
+                }
+
+                update();
+
+                MessageBox.Show(
+                    "The site has been blocked successfully!\n\nIt may take some time to apply changes.",
+                    "Success",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+            text.Text = "";
+        }
+
+        private void data_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var hittestInfo = data.HitTest(e.X, e.Y);
+
+                if (hittestInfo.Type == DataGridViewHitTestType.Cell)
+                {
+                    
+                    ContextMenuStrip contextMenu = new ContextMenuStrip();
+                    ToolStripMenuItem menuItem = new ToolStripMenuItem("Remove");
+                    
+                    menuItem.Image = Properties.Resources.Remove;
+
+                    menuItem.Click += Remove;
+                    contextMenu.Items.Add(menuItem);
+
+
+                    
+                    contextMenu.Show(data, new Point(e.X, e.Y));
+
+                }
+            }
+        }
+
+        private void Remove(object sender, EventArgs e)
+        {
+            
+            var menuItem = (ToolStripMenuItem)sender;
+            var contextMenu = (ContextMenuStrip)menuItem.Owner;
+            var dataGridView = (DataGridView)contextMenu.SourceControl;
+            var cell = dataGridView.CurrentCell;
+            string siteToUnblock = cell.Value.ToString();
+
+            string path = @"C:\Windows\System32\drivers\etc\hosts";
+            string hostsContent = File.ReadAllText(path);
+
+            string lineToRemove = "127.0.0.1\t" + siteToUnblock;
+            hostsContent = hostsContent.Replace(lineToRemove, "");
+
+            try
+            {
+                File.WriteAllText(path, hostsContent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                "ERROR # 1: The program should be run under admin privileges!!",
+                "Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error
+                );
+
+                Application.Exit();
+            }
+
+            
+            dataGridView.Rows.RemoveAt(cell.RowIndex);
+        }
+
+        private void text_Enter(object sender, EventArgs e)
+        {
+            info.Visible = true;
+            infoIcon.Visible = true;
+        }
+
+        private void text_Leave(object sender, EventArgs e)
+        {
+            info.Visible = false;
+            infoIcon.Visible = false;
+        }
+    }
+}
